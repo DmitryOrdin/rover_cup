70b5d8729b62714a6282f2773ae0c4c199c69680
DmitryOrdin <110549437+DmitryOrdin@users.noreply.github.com>
2026-04-01 20:04:34 +0300

    Update client.py

--- Programms/client.py
+++ Programms/client.py
@@ -1,20 +1,31 @@
 import socket
+import serial
+import threading
+import time
+import queue
 
 
 a = str(input("IP"))
 
-
 HOST = a  # IP-адрес сервера
 PORT = 4780 # Порт сервера
-
 rover_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 rover_client.connect((HOST, PORT))
-
-while(1):
-
-    message = (str(input()))
-    rover_client.sendall(message.encode('utf-8'))
-    print(f"Отправлено: {message}")
+msg = queue.Queue()
+port = "COM4"
+baud_rate = 9600
+ser = serial.Serial(port, baud_rate, timeout=1)
+def get_angles():
+    while True:
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8').rstrip()
+            msg.put(line)
+def send_angle():
+    while True:
+        message = msg.get()
+        rover_client.sendall(message.encode('utf-8'))
+        print(f"Отправлено: {message}")
+threading.Thread(target=get_angles(), args=(q,)).start()
+threading.Thread(target=send_angle(), args=(q,)).start()
 
 # Сокет автоматически закроется благодаря 'with'
